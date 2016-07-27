444354a45137d09c2d3e000db05766e40cd17525
murgler <devopsteam-dl@taskstream.com>
2016-07-27 12:07:38 -0400

    new skeleton
    totally not in my closet though

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,16 @@
+class skeleton {
+  file { '/etc/skel':
+    ensure => directory,
+    owner  => 'root',
+    group  => 'root',
+#    source => 'puppet:///modules/ssh/sshd_config',
+  }
+  
+  file { '/etc/skel/.bashrc':
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    require => File['/etc/skel'],
+    source  => 'puppet:///modules/site/skeleton/files/bashrc,
+  }
+}
