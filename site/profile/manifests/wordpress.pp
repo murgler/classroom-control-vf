819211979a88b93a293cfc148ae5a04f95e84aa3
murgler <devopsteam-dl@taskstream.com>
2016-07-29 16:06:00 -0400

    need quotes

--- site/profile/manifests/wordpress.pp
+++ site/profile/manifests/wordpress.pp
@@ -1,7 +1,7 @@
 class profile::wordpress {
   include apache
   class { wordpress:
-    install_dir => /var/www/html,
+    install_dir => '/var/www/html',
   }
   class {'mysql::server':
     root_password  => 'puppet',
